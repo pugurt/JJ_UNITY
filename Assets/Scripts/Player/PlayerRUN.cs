76e2305b668f29812cadc520d31e7c024025028b
pugurt <43779285+pugurt@users.noreply.github.com>
2018-11-03 16:55:14 +0900

    Add files via upload
    A1

---
+++ Assets/Scripts/Player/PlayerRUN.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRUN : PlayerFSMState
+{
+    public Transform marker;
+    public float movespeed = 3.0f;
+
+	void Start ()
+    {
+        marker = GameObject.FindGameObjectWithTag("Marker").transform;
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //transform.position = marker.position;
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            marker.position,
+            movespeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, marker.position) < 0.01f)
+        {
+            GetComponent<FSMManager>().SetState(PlayerState.IDLE);
+        }
+    }
+}
